90c18b5baaae1874ec41c1844b3e6aec5c4cbf3c
Schwarzer <wangzwnj@126.com>
2019-07-30 22:41:19 +0800

    Update

--- UsartGpu.h
+++ UsartGpu.h
@@ -77,6 +77,10 @@ public:
 	void Button(int hotspotId, int x1, int y1, int x2, int y2, int type, int ret)
 	{
 		SEND("BTN(%d,%d,%d,%d,%d,%d,%d);", hotspotId, x1, y1, x2, y2, type, ret);
+	} 
+	void ButtonReturn(int hotspotId, int ret)
+	{
+		SEND("TASC(%d,%d);", hotspotId, ret);
 	}
 	void ClearScreen(int colorCode = 0)
 	{
@@ -97,10 +101,22 @@ public:
 	void CurveProvideDataAndRefresh(uint8_t value)
 	{
 		SEND("S%d;", value);
+	} 
+	void DisplayInfo(int x, int y)
+	{
+		SEND("SNF(%d,%d);", x, y);
+	}
+	void GetInfo()
+	{
+		SEND("INF;");
 	}
 	void Icon(int x, int y, int pictureId, int xn, int yn, int id)
 	{
 		SEND("ICON(%d,%d,%d,%d,%d,%d);", x, y, pictureId, xn, yn, id);
+	}
+	void InputBufferDefault(const char* str)
+	{
+		SEND("TPIS('%s');", str);
 	}
 	void InputBufferDisplay(int size, int x, int y, int limitx, int lengthMax, int type, int colorCode, int lengthNeeded)
 	{
@@ -113,6 +129,10 @@ public:
 	void LabelPicture(int size, int x1, int y1, int x2, const char* str, int colorCode, int align, int backId)
 	{
 		SEND("PLAB(%d,%d,%d,%d,'%s',%d,%d,%d);", size, x1, y1, x2, str, colorCode, align, backId);
+	}
+	void Loopback(const char* str)
+	{
+		SEND("SEND('%s')", str);
 	}
 	void MenuInitialize(int x, int y, int width, int height, int iconsize, int xicon, int xtext, int textsize, int yn, int itemcount, int menupage, int reserved = 1)
 	{
@@ -205,6 +225,22 @@ public:
 	{
 		SEND("SCC(%d,%d);", colorCode, color);
 	}
+	void SetDirection(int direction)
+	{
+		SEND("DR%d;");
+	}
+	void ShowPage(int pageId)
+	{
+		SEND("SPG(%d);");
+	}
+	void SetBaseCoordinate(int x, int y)
+	{
+		SEND("SXY(%d,%d);", x, y);
+	}
+	void SetBackgroundLight(int light)
+	{
+		SEND("SEBL(%d);", light);
+	}
 	void String(int size, int x, int y, const char* str, int colorCode, int limitx = -1)
 	{
 		if (limitx == -1)
@@ -216,13 +252,44 @@ public:
 			SEND("DS%d(%d,%d,'%s',%d,%d);", size, x, y, str, colorCode, limitx);
 		}
 	}
+	void SwitchPictureButton(int hotspot, int x, int y, int width, int height, int backIdUp, int backIdDown, int ret)
+	{
+		SEND("PBTN(%d,%d,%d,%d,%d,%d,%d,%d);", hotspot, x, y, width, height, backIdUp, backIdDown, ret);
+	}
+	void Table(int x, int y, int cellwidth, int cellheight, int columns, int rows, int colorCode)
+	{
+		SEND("TABL(%d,%d,%d,%d,%d,%d,%d);", x, y, cellwidth, cellheight, columns, rows, colorCode);
+	}
+	void TableSetCell(int size, int row, int col, const char* str, int colorCode, int backgroundColorCode, int align)
+	{
+		SEND("CELS(%d,%d,%d,'%s',%d,%d,%d);", size, row, col, str, colorCode, backgroundColorCode, align);
+	}
+	void TableSetCellPictureBackground(int size, int row, int col, const char* str, int colorCode, int backId, int align)
+	{
+		SEND("CELP(%d,%d,%d,'%s',%d,%d,%d);", size, row, col, str, colorCode, backId, align);
+	}	
+	void TableUseCell(int row, int col)
+	{
+		SEND("CEUE(%d,%d);", row, col);
+	}
+	void TerminalMode()
+	{
+		SEND("TERM;");
+	}
 	void TouchScreenCalibration(int width, int height)
 	{
 		SEND("TPST(%d,%d);", width, height);
 	}
-	void TouchScreenMode(int mode, int default5key)
+	void TouchScreenMode(int mode, int default5key = -1)
 	{
-		SEND("TPN(%d,%d);", mode, default5key);
+		if (default5key != -1)
+		{
+			SEND("TPN(%d,%d);", mode, default5key);
+		}
+		else
+		{
+			SEND("TPN(%d);", mode);
+		}
 	}
 	void TouchScreenPencil(bool open, int colorCode)
 	{
@@ -234,5 +301,29 @@ public:
 		{ 
 			SEND("TPOW(255)");
 		}
+	}
+	void Windows8Box(int x1, int x2, int y1, int y2, int color)
+	{
+		SEND("W8BF(%d,%d,%d,%d,%d);", x1, x2, y1, y2, color);
+	} 
+	void Windows8TileScheme(int width, int height, const char * define)
+	{
+		SEND("W8DF(%d,%d,'%s');", width, height, define);
+	}
+	void Windows8TileRender(int x, int y, int singleTileWidth, int singleTileHeight, int spacing, int mode)
+	{
+		SEND("W8MU(%d,%d,%d,%d,%d,%d);", x, y, singleTileWidth, singleTileHeight, spacing, mode);
+	}
+	void Windows8TileUse(int tileId)
+	{
+		SEND("W8UE(%d);", tileId);
+	}
+	void Windows8TileSelect(int tileId)
+	{
+		SEND("W8SE(%d);", tileId);
+	}
+	void Windows8Hotspot(int hotspotId, int	x1, int y1, int x2, int y2, int color, int ret)
+	{
+		SEND("W8TP(%d,%d,%d,%d,%d,%d,%d);", hotspotId, x1, x2, y1, y2, color, ret);
 	}
 };
